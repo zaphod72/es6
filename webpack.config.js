9a61a190158bf459484b00ef118bcf3e9d6a77e0
Darren Kennedy <darrenk1972@gmail.com>
2017-10-15 23:16:02 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: ['./app/index.js'],
+  output: {
+    path: '/Users/darre/es6/build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    contentBase: './build',
+    inline: true
+  }
+}
